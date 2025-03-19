67e084c11363e3c12b38476d57163b6a25011aca
adonishenriquw <adonis.henrique21@gmail.com>
2025-03-19 20:50:47 -0300

    components dinamycs propertys

--- src/app/components/menu-tittle/menu-tittle.component.ts
+++ src/app/components/menu-tittle/menu-tittle.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-tittle',
   templateUrl: './menu-tittle.component.html',
-  styleUrls: ['./menu-tittle.component.css']
+  styleUrls: [
+    './menu-tittle.component.css',
+    './menu-tittle.responsive.component.css',
+  ],
 })
-export class MenuTittleComponent {
-
-}
+export class MenuTittleComponent {}
